ddbb8260d27047681b293f2ef0e0d3884aad7bad
NadaYounan <nadanouni1@gmail.com>
2022-09-06 17:42:15 +0200

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -257,3 +257,14 @@ $(".slide img").on("click" , function(){
   $(this).toggleClass('zoomed');
   $(".overlay").toggleClass('active');
 })
+
+/*recaptcha 
+var width = $('.g-recaptcha').parent().width();
+if (width < 302) {
+	var scale = width / 302;
+	$('.g-recaptcha').css('transform', 'scale(' + scale + ')');
+	$('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
+	$('.g-recaptcha').css('transform-origin', '0 0');
+	$('.g-recaptcha').css('-webkit-transform-origin', '0 0');
+}
+*/
